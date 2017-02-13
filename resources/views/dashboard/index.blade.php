f2bf307b4c852f9c10865c1536f4224a252c7634
moqiang02 <moqiang02@163.com>
2017-02-13 17:14:17 +0800

    提交数据到后台

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Example</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 </head>
 <body>
 <div id="app"></div>
+<script>
+    window.Laravel = <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+    ]); ?>
+</script>
 <script src="{{ elixir('js/app.js') }}"></script>
 
 </body>
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::group([
+    'namespace' => 'Api',
+], function () {
+
+    Route::resource('user', 'UserController');
+
+});
+
